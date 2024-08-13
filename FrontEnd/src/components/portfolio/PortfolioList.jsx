fc6e1f936868902bf128f5e041a4f70fa09d375d
John King <john.king@renlearn.co.uk>
2024-08-13 12:33:33 +0100

    added one small comment

--- FrontEnd/src/components/portfolio/PortfolioList.jsx
+++ FrontEnd/src/components/portfolio/PortfolioList.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import PortfolioCard from "./PortfolioCard";
 
 const PortfolioList = () => {
-  const [portfolios, setPortfolios] = useState([{ id: 1, portfolioName: "Portfolio 1", totalInvestment: 1000, pAndL: 200, percPAndL: "20%", lastUpdated: "2021-09-01" }, { id: 2, portfolioName: "Portfolio 2", totalInvestment: 2000, pAndL: 400, percPAndL: "20%", lastUpdated: "2021-09-01" }, { id: 3, portfolioName: "Portfolio 3", totalInvestment: 3000, pAndL: 600, percPAndL: "20%", lastUpdated: "2021-09-01" }]);
+  const [portfolios, setPortfolios] = useState([{ id: 1, portfolioName: "Portfolio 1", totalInvestment: 1000, pAndL: 200, percPAndL: "20%", lastUpdated: "2021-09-01" }, { id: 2, portfolioName: "Portfolio 2", totalInvestment: 2000, pAndL: 400, percPAndL: "20%", lastUpdated: "2021-09-01" }, { id: 3, portfolioName: "Portfolio 3", totalInvestment: 3000, pAndL: 600, percPAndL: "20%", lastUpdated: "2021-09-01" }]); // Hardcoded for now
   return (
     <>
       <div className="portfolios">
