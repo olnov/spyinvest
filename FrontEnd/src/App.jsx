65a2d203a7e658eb2fc58aab03b9aafd9d6929cf
Joey Allen <joey.allen@renlearn.co.uk>
2024-08-14 17:10:20 +0100

    adding the portfolio and asset components and add asset form

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PortfolioAsset from './components/PortfolioAsset'
 import AllAssets from './pages/AllAssets'
+import AddAsset from './components/AddAsset'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <AllAssets/>
+      <AddAsset/>
     </>
   )
 }
--- FrontEnd/src/components/PortfolioAsset.jsx
+++ FrontEnd/src/components/PortfolioAsset.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
-const PortfolioAsset = ({portfolio_id, asset_id, date_purchased, date_sell, quantity, buying_price, selling_price}) => {
+const PortfolioAsset = ({portfolio_id, asset_id, buying_price, selling_price}) => {
   return (
     <>
     <h1>Portfolio Asset</h1>
     <div>{portfolio_id}</div>
     <div>{asset_id}</div>
-    <div>{date_purchased}</div>
-    <div>{date_sell}</div>
-    <div>{quantity}</div>
     <div>{buying_price}</div>
     <div>{selling_price}</div>
     </>
