eb49483fe6dc914a914d723c10903fbf65b8d3e0
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-19 23:20:22 +0100

    chore: Refactor assetChecker middleware and handle asset not found case

--- API/middleware/assetChecker.js
+++ API/middleware/assetChecker.js
@@ -1,15 +1,24 @@
-const JWT = require("jsonwebtoken");
 const Asset = require("../models/Asset");
-const { createAsset } = require("../controllers/Assets");
 
-// Middleware function to check for valid tokens
-const assetChecker = (req, res, next) => {
-    const asset_name = req.body.asset_name;
-    if (Asset.findOne({ where: { asset_name: asset_name } })) {
-        next();
+// Middleware function to check for existing assets in the database
+const assetChecker = async (req, res, next) => {
+    try {
+        const reqAsset = req.body.asset_symbol;
+        // Await the result of the database query
+        const asset = await Asset.findOne({ where: { asset: reqAsset } });
+        
+        if (asset) {
+            // Add the asset ID to the req object.
+            req.asset_id = asset.id;
+            next();
+        } else {
+            res.status(404).json({ message: "Asset not found" });
+            // TODO ON FE - when you get this response enable the user to manually fill the asset form?
+        }
+    } catch (error) {
+        console.error('Error in assetChecker: ', error);
+        res.status(500).json({ message: "Internal server error" });
     }
-    else {
-        // const asset = api.fetch;//tbc//
-    }   
-}
+};
+
 module.exports = assetChecker;
