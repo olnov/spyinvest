517916745bc03f1fafd9872c78747a4e13fa514f
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-19 14:51:51 +0100

    Wrapping fetch request for SEC with try / catch err-

--- API/seedAssets.js
+++ API/seedAssets.js
@@ -4,70 +4,29 @@ const Asset = require('./models/Asset');
 
 
 const fetchAssetsData = async () => {
+try {
     const request = await fetch("https://www.sec.gov/files/company_tickers.json");
+    if (request){
+        console.log('I have received request.')
+    }
     const response = await request.json();
+    console.log("Response:", response);
     const count = Object.keys(response).length;
     console.log("Lenght:", count);
     await sequelize.sync({ force: true });
     for (var i = 0; i < count; i++) {
-
-
 
         const asset = await Asset.create({
             asset: response[i].ticker,
             description: response[i].title
         })
 
-        // console.log("Ticker:", response[i].ticker);
-        // console.log("Description:", response[i].title)
+        console.log("Ticker:", response[i].ticker);
+        console.log("Description:", response[i].title)
     }
+} catch (error) {
+    console.error("Error fetching data:", error);
+}
 }
 
 fetchAssetsData();
-
-
-const seedDatabase = async () => {
-
-    // const SECseedURL = await fetch("https://www.sec.gov/files/company_tickers.json")
-    // const serialized = await SECseedURL.json()
-    // // console.log(serialized)
-    // for (const property in serialized) {
-    //     console.log(`${property}: ${serialized["ticker"]}`)
-    // }
-
-    // Use fetch to get the JSON data from the URL
-    // fetch('https://www.sec.gov/files/company_tickers.json')
-    //     .then(response => response.json()) // Parse the response as JSON
-    //     .then(data => {
-    //         // Extract the tickers
-    //         const tickers = Object.values(data).map(company => company);
-    //     })
-    //     .catch(error => console.error('Error fetching the data:', error));
-
-    // const getCompTickers = async () => {
-    //     const compDetails = await fetch('https://www.sec.gov/files/company_tickers.json')
-    //     const result = await compDetails.json()
-    //     const tickers = Object.values(result).map(company => company.ticker);
-    //     console.log(tickers)
-    //     // return result
-    // }
-
-    // getCompTickers()
-
-    // for (let k = 0; k < tickers.length; k++) {
-    //     let assetTicker = tickers[k].ticker
-    //     let assetTitle = tickers[k].title
-    //     console.log(assetTicker)
-    // const asset = await Asset.create({
-    //     asset: assetTicker,
-    //     description: assetTitle
-    // });
-    // assets.push(asset);
-    // }
-
-
-}
-// seedDatabase();
-
-
-
