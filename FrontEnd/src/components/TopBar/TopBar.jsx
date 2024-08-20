58e41180428dc689afb8836a964d6570ec5a81c0
olnov <o.novikov@ymail.com>
2024-08-20 09:42:35 +0100

    Added topbar component

---
+++ FrontEnd/src/components/TopBar/TopBar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Logo from "../../assets/logo-si-5.png";
+import { useNavigate } from "react-router-dom";
+
+export const TopBar = ()=>{
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('userId');
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
+
+
+    return (
+        <>
+        {/* <nav className="navbar fixed-top navbar-light bg-light shadow-sm p-0 mb-5 bg-body-tertiary"> */}
+        <nav className="navbar navbar-expand-lg fixed-top shadow-sm navbar-light bg-light">
+        <div className="d-flex align-items-center" style={{ marginLeft: '30px'}}>
+        <a className="navbar-brand" href="#">
+        <img src={Logo} width="108" height="42" alt="" />
+        </a>
+        </div>
+        { !token ? (
+        <>
+        <div className="d-flex">
+            <a href="#" className="btn btn-outline-primary me-2">Login</a>
+            <a href="#" className="btn btn-primary">Signup</a>
+        </div>
+        </>
+        ) : (
+        <>
+        <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link fs-5 link-underline-opacity-100-hover" aria-current="page" href="/portfolio">Portfolio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link fs-5" aria-current="page" href="/dashboard">My dashboard</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link fs-5" aria-current="page" href="/profile">Profile settings</a>
+                </li>
+            </ul>
+        </div>
+        </div>
+        <div className="d-flex">
+            <button className="btn btn-outline-primary me-2" onClick={handleLogout}>Logout</button>
+        </div></>  
+        )}
+        </nav>
+        </>
+    )
+}
